50c81c473b6a692da201d7d461ae68e6341cdcd3
sudbala <sudharsan.bala@yahoo.com>
2020-12-09 11:53:40 -0500

    map

--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -1,4 +1,5 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:flutter/material.dart';
 
 import 'MainScreen.dart';
@@ -9,13 +10,20 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MediaQuery(
+      // Have to wrap in a media query so that the animated splash screen
+      // understands how to fill up the space. Media queries return data about
+      // the specific screen size and dimensions of the device.
       data: new MediaQueryData(),
       child: MaterialApp(
+        // Has a duration, image, and transition types to the next screen.
         home: AnimatedSplashScreen(
+            duration: 2000,
             splash: 'images/mountain.png',
-            nextScreen: MainScreen(title: 'Atlas'),
+            nextScreen: MainScreen(),
             splashTransition: SplashTransition.fadeTransition,
+            pageTransitionType: PageTransitionType.bottomToTop,
         ),
       ),
     );
